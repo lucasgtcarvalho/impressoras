ddb579e45879183f839cb6cb0bd6e4aad775666c
psyluks <62725949+lucasgtcarvalho@users.noreply.github.com>
2026-06-25 14:59:41 -0300

    Create Program.cs

---
+++ agent/PrintMonitor.Agent/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using PrintMonitor.Agent.Services;
+using PrintMonitor.Agent.Collectors;
+using PrintMonitor.Agent.Sync;
+using PrintMonitor.Agent.Storage;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/agent-.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+try
+{
+    var host = Host.CreateDefaultBuilder(args)
+        .UseWindowsService(options =>
+        {
+            options.ServiceName = "PrintMonitor Agent";
+        })
+        .UseSerilog()
+        .ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<ConfigManager>();
+            services.AddSingleton<TokenManager>();
+            services.AddSingleton<LocalDatabase>();
+            services.AddSingleton<NetworkDiscoveryService>();
+            services.AddSingleton<SnmpCollectorService>();
+            services.AddSingleton<JobCollectorService>();
+            services.AddSingleton<SyncEngine>();
+            services.AddSingleton<HeartbeatService>();
+
+            services.AddHttpClient("AgentApi")
+                .ConfigureHttpClient((sp, client) =>
+                {
+                    var config = sp.GetRequiredService<ConfigManager>();
+                    client.BaseAddress = new Uri(config.ServerUrl);
+                    client.Timeout = TimeSpan.FromSeconds(30);
+                });
+
+            services.AddHostedService<Worker>();
+        })
+        .Build();
+
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Agent terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
