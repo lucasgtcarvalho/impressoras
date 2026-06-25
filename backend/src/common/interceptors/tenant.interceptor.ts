278caf70eee3edd93770fa764e906b64b54718f1
psyluks <62725949+lucasgtcarvalho@users.noreply.github.com>
2026-06-25 14:58:54 -0300

    Create tenant.interceptor.ts

---
+++ backend/src/common/interceptors/tenant.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TenantInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (user && user.clientIds && user.clientIds.length > 0) {
+      request.allowedClientIds = user.clientIds;
+    }
+
+    return next.handle();
+  }
+}
